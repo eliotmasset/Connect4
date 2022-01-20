5917985d57943b6d7846b58063645f9642323471
Eliot Masset <58653158+eliotmasset@users.noreply.github.com>
2022-01-20 13:46:25 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import regeneratorRuntime from "regenerator-runtime";
 import css from "./connect4.css";
 import body from "./connect4.html";
 import { Controller } from "./Controller/Controller.js";
-import { View } from "./View/View.js";
-import { Model } from "./Model/Model.js";
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 //Initialisation des variables :
 document.body.innerHTML = body;
-const app = new Controller(new Model(), new View("connect4Canvas"));
+const app = new Controller();
